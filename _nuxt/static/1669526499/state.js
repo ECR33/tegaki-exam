188afeee2254609c73d9634d79a6d31a0338ac5d
ECR33 <ECR33@users.noreply.github.com>
2022-11-27 05:21:41 +0000

    deploy: 904c97a5bbdcc19eabb42d40b4e396c472169497

---
+++ _nuxt/static/1669526499/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ftegaki-exam\u002F_nuxt\u002Fstatic\u002F1669526499",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Ftegaki-exam\u002F",assetsPath:"\u002Ftegaki-exam\u002F_nuxt\u002F",cdnURL:a}}}}(null));
