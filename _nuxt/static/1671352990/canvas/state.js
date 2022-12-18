49b9b9c9ebead1b2e8ee11bcacd4949bf5aec000
ECR33 <ECR33@users.noreply.github.com>
2022-12-18 08:43:13 +0000

    deploy: 3929c61046e5b970253308f494f1bf2af2e9074c

---
+++ _nuxt/static/1671352990/canvas/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ftegaki-exam\u002F_nuxt\u002Fstatic\u002F1671352990",layout:"pyper",error:a,serverRendered:true,routePath:"\u002Fcanvas",config:{_app:{basePath:"\u002Ftegaki-exam\u002F",assetsPath:"\u002Ftegaki-exam\u002F_nuxt\u002F",cdnURL:a}}}}(null));
